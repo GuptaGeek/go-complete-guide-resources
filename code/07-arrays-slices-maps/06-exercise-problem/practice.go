cc0850b762c59b2671b677a1a658ccdc602e27cb
sheetal gupta <150527877+GuptaGeek@users.noreply.github.com>
2025-02-07 15:51:56 +0530

    Update practice.go

--- code/07-arrays-slices-maps/06-exercise-problem/practice.go
+++ code/07-arrays-slices-maps/06-exercise-problem/practice.go
@@ -1,7 +1,40 @@
 package main
+type product struct{
+  title string
+  id string
+  price int
+  }
 
 func main() {
+   hobbies:=[3]string{"badminton","dancing","scrolling"}
+   fmt.Println(hobbies)
 
+  fmt.Println(hobbies[0])
+  fmt.Println(hobbies[1:3])
+
+  mainhobbies1:=[] string { hobbies[0],hobbies[1] }
+  fmt.Println(mainhobbies1)
+
+  mainhobbies2:= make( []string , hobbies[0])
+  mainhobbies2 = append(mainhobbies,hobbies[1])
+  fmt.Println(mainhobbies2)
+
+  mainhobbies2= hobbies[1:3]
+  fmt.Println(mainhobbies2)
+
+  dynamic_Arrays:= []string{"tolearngo","tomakeaproject"}
+  fmt.Pritnln(dynamic_Arrays)
+
+
+  dynamic_Arrays[1]="tomakelotsofproject"
+  dynmain_Arrays= append(dynamic_Arrays,"tomastergo"}
+                         
+  product_list1:=[]product{ product{title:"vanila",id:"67",price:99},product{title:"chocolate",id:"89",price:65}
+  fmt.Println(product_list1)
+
+  
+  
+  
 }
 
 // Time to practice what you learned!
